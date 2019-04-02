f468a88fcd5b98b9113037927af5f8ca8d6cad81
zhiyoucai <zq.works@hotmail.com>
2019-04-02 11:14:25 +0800

    规整了代码，实现了一个数据库循环转换

--- mysqlreader.py
+++ mysqlreader.py
@@ -1,13 +1,11 @@
 import pymysql
 import re
+import argparse
 
-conn = pymysql.connect(host="127.0.0.1", user="root", password="123456", db="test")
-cursor = conn.cursor()
-
-re_key_1 = re.compile(r'KEY `(\w+)` \((.*)\)')
-re_key_2 = re.compile(r'UNIQUE KEY `(\w+)` \((.*)\)')
 
 def convert_index_ddl(table_name):
+    re_key_1 = re.compile(r'KEY `(\w+)` \((.*)\)')
+    re_key_2 = re.compile(r'UNIQUE KEY `(\w+)` \((.*)\)')
     cursor.execute("show create table %s" % table_name)
     rows = cursor.fetchall()
     ddl = rows[0][1]
@@ -19,11 +17,10 @@ def convert_index_ddl(table_name):
         if key_match:
             i_ddl += "create index " + key_match.group(1) + " on " + table_name + "(" \
                      + key_match.group(2).replace("`", "") + ");\n"
-            #print(i_ddl)
         elif ukey_match:
             i_ddl += "alter table " + table_name + " add CONSTRAINT " + ukey_match.group(1) \
                      + " unique (" + ukey_match.group(2).replace("`", "") + ");\n"
-    f = open("%s.sql" % table_name, "w")
+    f = open("%s.sql" % table_name, "a")
     f.write(i_ddl)
     f.close()
 
@@ -31,17 +28,19 @@ def convert_index_ddl(table_name):
 def convert_table_ddl(table_name):
     cursor.execute("show full columns from %s" % table_name)
     rows = cursor.fetchall()
-    f = open("dest.sql", "w")
+    f = open("%s.sql" % table_name, "w")
 
     f.write("create table %s (\n" % table_name)
     i = 0
+    d_def = ""
     for row in rows:
         col_name = row[0]
         col_def = row[1]
         col_nullable = row[3]
         col_default = row[5]
         col_pk = row[4]
-        col_comm = row[8]
+
+        # col_comm = row[8]
         if col_def.startswith("int", 0, 3):
             d_def = "number(10)"
         elif col_def.startswith("bigint", 0, 6):
@@ -76,8 +75,39 @@ def convert_table_ddl(table_name):
         if i < len(rows):
             line += ","
         f.write(line + "\n")
-    f.write(");")
+    f.write(");\n")
     f.close()
 
-convert_table_ddl("test")
-convert_index_ddl("test")
+
+def get_tables():
+    cursor.execute("show tables")
+    tables = cursor.fetchall()
+    for table in tables[0]:
+        convert_table_ddl(table)
+        convert_index_ddl(table)
+
+
+def _argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--host", help="键入host地址")
+    parser.add_argument("-u", "--user", help="type user name")
+    parser.add_argument("-p", "--passwd", help="type password")
+    parser.add_argument("-d", "--db", help="type db name")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = _argparse()
+    host = args.host
+    user = args.user
+    password = args.passwd
+    database = args.db
+    conn = pymysql.connect(host=host, user=user, password=password, db=database)
+    global cursor
+    cursor = conn.cursor()
+    get_tables()
+
+
+if __name__ == '__main__':
+    main()
