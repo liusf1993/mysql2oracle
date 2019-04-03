a6ec1beb7703c34e5ea568888b7b251b0c7faac9
zhiyoucai <zq.works@hotmail.com>
2019-04-03 14:05:26 +0800

    解决掉了实际测试过程中发现的所有问题

--- mysqlreader.py
+++ mysqlreader.py
@@ -13,21 +13,21 @@ def convert_index_ddl(table_name):
     i_ddl = ""
     for line in ddl.split("\n"):
         # 一般索引
-        key_match = re_key_1.match(line.strip())
-        # 唯一索引
-        ukey_match = re_key_2.match(line.strip())
-        # 主键索引
-        pkey_match = re_key_3.match(line.strip())
+        key_match = re_key_1.search(line)
+        pkey_match = re_key_3.search(line)
         if key_match:
-            i_ddl += "create index " + key_match.group(1) + " on " + table_name + "(" \
-                     + key_match.group(2).replace("`", "") + ");\n"
-        elif ukey_match:
-            i_ddl += "alter table " + table_name + " add CONSTRAINT Un_" + table_name[2:25] \
-                     + " unique (" + ukey_match.group(2).replace("`", "") + ");\n"
+            key_name = key_match.group(1)
+            key_props = key_match.group(2).replace("`", "")
+            if "UNIQUE" in line:
+                i_ddl += "alter table " + table_name + " add constraint " + key_name + " unique (" + key_props + ");\n"
+            else:
+                i_ddl += "create index " + key_name + " on " + table_name + "(" \
+                         + key_props.replace("`", "") + ");\n"
         elif pkey_match:
-            i_ddl += "alter table " + table_name + " add CONSTRAINT pk_" + table_name[2:25] \
-                     + " primary key (" + pkey_match.group(1).replace("`", "") + ");\n"
-    f = open("out/%s.sql" % table_name, "a")
+            key_props = pkey_match[1].replace("`", "")
+            i_ddl += "alter table " + table_name + " add constraint P_" + table_name[2:] \
+                     + " primary key (" + key_props + ");\n"
+    f = open("out/%s.tab" % table_name, "a")
     f.write(i_ddl)
     f.close()
 
@@ -35,7 +35,7 @@ def convert_index_ddl(table_name):
 def convert_table_ddl(table_name):
     cursor.execute("show full columns from %s" % table_name)
     rows = cursor.fetchall()
-    f = open("out/%s.sql" % table_name, "w")
+    f = open("out/%s.tab" % table_name, "w")
 
     f.write("create table %s (\n" % table_name)
     i = 0
@@ -50,12 +50,15 @@ def convert_table_ddl(table_name):
         elif col_def.startswith("bigint", 0, 6):
             d_def = "number(20)"
         elif col_def.startswith("varchar", 0, 7):
-            if int(col_def[7:].replace("(", "").replace(")", "")) * 3 <= 1024:
+            temp = int(col_def[7:].replace("(", "").replace(")", ""))
+            if temp * 3 <= 1024:
                 d_length = int(col_def[7:].replace("(", "").replace(")", "")) * 3
+            elif temp > 4000:
+                d_length = 4000
             else:
                 d_length = int(col_def[7:].replace("(", "").replace(")", ""))
             d_def = "varchar2(" + str(d_length) + ")"
-        elif col_def == "blob" or col_def == "mediumblob":
+        elif col_def == "blob" or col_def == "mediumblob" or col_def == "longblob":
             d_def = "blob"
         elif col_def.startswith("decimal"):
             d_def = "number" + col_def[7:]
@@ -65,16 +68,22 @@ def convert_table_ddl(table_name):
             d_def = "number(4)"
         elif col_def in ("longtext", "text"):
             d_def = "clob"
+        elif col_def.startswith("bit"):
+            d_def = "number(1)"
         else:
             print("暂不支持该类型转换，请等待工具升级")
-            print(col_def)
+            print(table_name + ":" + col_def)
         line = col_name + " " + d_def
         if col_default is not None:
             if col_default == "CURRENT_TIMESTAMP":
                 line += " default sysdate"
+            elif col_def.startswith("date") or col_def.startswith("datetime") or col_def.startswith("timestamp"):
+                line += " default to_date('" + col_default + "', 'yyyy-mm-dd hh24:mi:ss')"
             # Oracle会将空字符串自动变成null，因此不允许出现default ''，这里特殊处理
             elif col_default == "":
                 line += ""
+            elif d_def.startswith("number"):
+                line += " default " + col_default
             else:
                 line += " default '" + col_default + "'"
         if col_nullable == "NO":
